36a129e2be0c1dd0e1584cd5fd320a8c937dba9b
Pedro Mauricio Ilarrescape <ilarrescapem@gmail>
2026-06-22 21:20:26 -0300

    creacion primera tabla

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -2,3 +2,7 @@ create database if not exists sistema_restaurante;
 use sistema_restaurante;
 -- hola mundo
 
+create table if not exists una_tabla(
+    id_campo int not null auto_increment,
+    nombre_campo varchar(45) not null
+);
