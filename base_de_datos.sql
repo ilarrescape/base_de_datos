5cf24516565694685edacfd26070b6678031e21e
Pedro Mauricio Ilarrescape <ilarrescapem@gmail>
2026-06-22 21:35:50 -0300

    Hola Mundo

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,6 +1,9 @@
 create database if not exists sistema_restaurante;
 use sistema_restaurante;
 
+
+-- hola mundo
+
 create table Area (
     id_area int AUTO_INCREMENT primary key,
     nombre_area varchar(50) not null
