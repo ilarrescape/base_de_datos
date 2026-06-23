0c6fce18a2ba1b962f2e99d1a8f94ed262d79d37
Diego <diegobelizanvillagesell@gmail.com>
2026-06-22 21:11:09 -0300

    Agregue una tabla de prueba

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,2 +1,7 @@
 create database if not exists sistema_restaurante;
 use sistema_restaurante;
+
+create table Area (
+    id_area int AUTO_INCREMENT primary key,
+    nombre_area varchar(50) not null
+);
