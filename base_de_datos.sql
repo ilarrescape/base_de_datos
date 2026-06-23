407c8b1c2d39bd6aa8febf7bdaa3557fb2cc5dec
Pedro Mauricio Ilarrescape <ilarrescapem@gmail>
2026-06-22 21:42:58 -0300

    Comentarios

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -8,3 +8,7 @@ create table Area (
     id_area int AUTO_INCREMENT primary key,
     nombre_area varchar(50) not null
 );
+-- PARA Poder unir dos ramas, debo ingresar a una rama secundaria y unirme usando el nombre de la otra rama
+-- Si yo quiero unir rama-1 con main
+-- Me muevo a rama-1
+-- y escribimos git merge main
