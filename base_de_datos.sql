3cf04a65d5c7f7a1a83b56e326b4dedf4466fc8f
Diego <diegobelizanvillagesell@gmail.com>
2026-07-06 16:32:40 -0300

    Agregue la ultima tabla que me quedaba hacer, mi rama es la tabla1 que la hice hace rato por eso quedo el nombre asi

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,11 +1,17 @@
 create database if not exists sistema_restaurante;
 use sistema_restaurante;
-<<<<<<< HEAD
 
 create table Area (
     id_area int AUTO_INCREMENT primary key,
     nombre_area varchar(50) not null
 );
-=======
 -- hola mundo
->>>>>>> 2494661bb1b715825d54e00c37a42c8d4cb65651
+
+
+-- hola mundo
+
+create table Estado_condicion (
+    id_estado_condicion INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_estado_condicion VARCHAR(100) NOT NULL,
+    empresa_de_dependencia VARCHAR(100) NOT NULL
+)
