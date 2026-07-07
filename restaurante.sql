a641930a968508d7d434b6eb57094d93000afbc0
Pedro Mauricio Ilarrescape <ilarrescapem@gmail>
2026-07-06 21:41:46 -0300

    Modificaciones en las tablas pedido y detalle_pedido

--- restaurante.sql
+++ restaurante.sql
@@ -14,23 +14,39 @@ CREATE TABLE pedido(
     total_pedido DOUBLE NOT NULL,
 
     FOREIGN KEY (fk_tipo_comprobante) 
-      REFERENCES tipo_comprobante(id_tipo_comprobante),
+      REFERENCES tipo_comprobante(id_tipo_comprobante)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT,
 
     FOREIGN KEY (fk_cuit_restaurante) 
-      REFERENCES restaurante(cuit),
+      REFERENCES restaurante(cuit)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT,
 
     FOREIGN KEY (fk_empleado)
-      REFERENCES empleado(id_empleado),
+      REFERENCES empleado(id_empleado)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT,
 
     FOREIGN KEY (fk_dni_cliente)
-      REFERENCES cliente(dni),
+      REFERENCES cliente(dni)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT,
     
     FOREIGN KEY (fk_mesa)
-      REFERENCES mesas(id_mesa),
+      REFERENCES mesas(id_mesa)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT,
 
     FOREIGN KEY(fk_estado)
       REFERENCES estado_pedido(pk_estado)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT
 );
+
+
+
+
 
 CREATE TABLE detalle_pedido(
     fk_producto INT,
@@ -39,7 +55,24 @@ CREATE TABLE detalle_pedido(
     subtotal DOUBLE NOT NULL,
 
     PRIMARY KEY (fk_producto, fk_pedido),
-    FOREIGN KEY (fk_producto) REFERENCES producto(id_producto),
+    FOREIGN KEY (fk_producto) REFERENCES producto(id_producto)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT,
+    
     FOREIGN KEY (fk_pedido) REFERENCES pedido(id_pedido)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
+# Producto
+# Mila Napolitana
+# Pizza Napolitana
+# Mila a Caballo
+# Muzzarella
+# Pizza Primavera
+# Rabas
+# Papas Fritas
+
+#Pedido 12340
+# 2 Mila Napolitana
+# 2 Fritas
